1b446cc07d6ebd89b543648db9330ff3a8ba90da
weiyinglei <weiyinglei@live.com>
2016-11-01 18:55:44 +0800

    io,file

---
+++ src/main/java/com/wei/corejava/core4/FileResearch.java
@@ -0,0 +1,31 @@
+package com.wei.corejava.core4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileResearch
+{
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws FileNotFoundException {  
+        Scanner in = new Scanner(new File("G:\\in.txt"));  
+        PrintWriter pw = new PrintWriter(new File("G:\\file.txt"));  
+        String name = "GinSmile";  
+        pw.write(name);//把字符串添加到文件末尾  
+        pw.append('w');//直接把字符添加到文件末尾  
+        pw.println();//仅仅打印一个换行符  
+        while(in.hasNext()){  
+            String s = in.nextLine();  
+            pw.println(s);  
+        }  
+          
+        pw.println();  
+        pw.printf("%x", 100);  
+        pw.println();  
+        char[] buf = {'a','b','c','d','e','f'};  
+        pw.write(buf, 0, 4);//把字符数组的一部分添加到文件末尾  
+          
+        pw.close();  
+    }  
+}
