6b029a2daf2decbdf69a1280d35991e8feaac16b
weiyinglei <weiyinglei@live.com>
2016-11-03 09:24:02 +0800

    helloworld

---
+++ README.md
@@ -0,0 +1,10 @@
+#javaRearch
+
+corejava
+从helloworld到各个节点的重点演示
+
+pkg
+各种官方类库
+
+domain
+各种第三方组件
---
+++ src/main/java/corejava/core2/HelloWorld.java
@@ -0,0 +1,9 @@
+package corejava.core2;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
